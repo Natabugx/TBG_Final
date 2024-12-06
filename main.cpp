029e126669d80d2d1821978af2f951fa3b261b50
Natabugx <150644652+Natabugx@users.noreply.github.com>
2024-12-06 10:12:59 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-const enum ROOMS{ENTRANCE=7,FOOD_COURT=8, CARTS=9,CHECKOUT=10,PRODUCE =11,PHARMACY=12,BAKERY=13,BACK_ROOMS=14};
+const enum ROOMS { ENTRANCE = 7, FOOD_COURT = 8, CARTS = 9, CHECKOUT = 10, PRODUCE = 11, PHARMACY = 12, BAKERY = 13, BACK_ROOMS = 14 };
 
 int main() {
 	int room = 1;
@@ -8,24 +8,6 @@ int main() {
 
 	while (input != "quit") {
 		switch (room) {
-		case 1: //
-		
-		case FOOD_COURT://Food court
-			
-		case CARTS: //Carts
-			
-		case CHECKOUT://Checkout
-			cout << "You are in ___, you can go" << endl;
-			cin >> input;
-			else
-				cout << "sorry, thats not an option" << endl;
-			break;
-		case 1://aisle 1
-			cout << "You are in ___, you can go" << endl;
-			cin >> input;
-			else
-				cout << "sorry, thats not an option" << endl;
-				break;
 		case ENTRANCE://Entrance
 			cout << "you are walking through the entrence, you can go west, north and east" << endl;
 			cin >> input;
@@ -38,34 +20,68 @@ int main() {
 			else
 				cout << "sorry, thats not an option" << endl;
 			break;
-		case 8://aisle 9
+
+		case FOOD_COURT://Food court
 			cout << "you are in the food court, you can go north and west" << endl;
 			cin >> input;
 			if (input == "north")
 				room = 6;
-			else if (input == "west")
-				room = 1;
+			if (input == "west")
+				room = ENTRANCE;
 			else
 				cout << "sorry, thats not an option" << endl;
 			break;
-		case CARTS://carts
-			cout << "you are by the carts, you can go west, north, east" << endl;
+
+		case CARTS: //Carts
+			cout << "you are by the carts, you can go west, north, and east" << endl;
 			cin >> input;
 			if (input == "west")
-				room = 4;
-			else if (input == "north")
-				room = 8;
-			else if (input == "east")
-				room = 1;
+				room = CHECKOUT;
+			if (input == "north")
+				room = PRODUCE;
+			if (input == "east")
+				room = ENTRANCE;
 			else
 				cout << "sorry, thats not an option" << endl;
 			break;
+
 		case CHECKOUT://Checkout
-			cout << "You are in ___, you can go" endl;
+			cout << "You are in the checkout, you can go north and east" << endl;
 			cin >> input;
+			if (input == "north")
+				room = 3;
+			if (input == "east")
+				room = CARTS;
 			else
 				cout << "sorry, thats not an option" << endl;
 				break;
+
+		case PRODUCE:
+			cout << "You are in the produce, you can go west, east, south" << endl;
+			cin >> input;
+			if (input == "west") {
+				cout << "Do you want to go in Aisle 1, 2, or 3?"
+					if (input == "1")
+						room = 1;
+
+					if (input == "2")
+						room = 2;
+
+					if (input == "3")
+						room = 3;
+			}
+				
+			if (input == "east") {
+				cout << "Do you want to go in Aisle 4, 5, or 6?"
+					if (input == "4")
+						room = 4;
+					if (input == "5")
+						room = 5;
+					if (input == "6")
+						room = 6;
+			}
+			if (input == "south")
+				room = CARTS;
 		}
 	}
 }
