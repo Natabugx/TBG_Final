97e44609a4412243fd7449a782170b41d2bc02d8
Natabugx <150644652+Natabugx@users.noreply.github.com>
2024-12-16 12:04:28 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,36 @@
 #include<iostream>
 #include<cstdio>
+#include<chrono>
 using namespace std;
 const enum ROOMS { ENTRANCE = 7, FOOD_COURT = 8, CARTS = 9, CHECKOUT = 10, PRODUCE = 11, PHARMACY = 12, BAKERY = 13, BACK_ROOMS = 14, START = 15 };
 
 void ListGen();
 
 int main() {
+	const int TIME_LIMIT = 600; //Time limit in seconds
+	auto start = chrono::steady_clock::now(); // Record start time
+	bool timerRunning = false;
+	int timeLeft = 600;
+	//int room = 1;
+	//char input;
 
-	ListGen();
-	int room = ENTRANCE;
+
+	int room = START;
+	int health = 12;
+
+
 	string input;
 
-	while (input != "quit") {
+	while (input != "quit" || timeLeft > 0) {
+
+		if (timerRunning == true) {
+			auto now = chrono::steady_clock::now();
+			timeLeft = TIME_LIMIT - chrono::duration_cast<chrono::seconds>(now - start).count();
+			cout << "Time left: " << timeLeft/60 << " minutes " << timeLeft%60 << " seconds" << "     ";//Show remaining time
+		}
+
+		health--;
+		cout << "Health: " << health << endl;
 		switch (room) {
 
 		case 1: //Aisle 1
@@ -24,8 +43,10 @@ int main() {
 				room = PRODUCE;
 			else if (input == "south")
 				room = 2;
-			else
+			else {
 				cout << "Sorry, thats not an option" << endl;
+				health++;
+			}
 			break;
 
 		case 2: //Aisle 2
@@ -38,8 +59,10 @@ int main() {
 				room = PRODUCE;
 			else if (input == "south")
 				room = 3;
-			else
-				cout << "Sorr, thats not an option" << endl;
+			else {
+				cout << "Sorry, thats not an option" << endl;
+				health++;
+			}
 			break;
 
 		case 3: //Aisle 3
@@ -52,10 +75,11 @@ int main() {
 				room = PRODUCE;
 			else if (input == "south")
 				room = CHECKOUT;
-			else
+			else {
 				cout << "Sorry, thats not an option" << endl;
+				health++;
+			}
 			break;
-
 		case 4:
 			cout << "You are in Aisle 4, You can go north, west, or south" << endl;
 			cin >> input;
@@ -66,10 +90,11 @@ int main() {
 				room = PRODUCE;
 			else if (input == "south")
 				room = 5;
-			else
+			else {
 				cout << "Sorry, thats not an option" << endl;
+				health++;
+			}
 			break;
-
 		case 5:
 			cout << "You are in Aisle 5, You can go north, west, or south" << endl;
 			cin >> input;
@@ -80,10 +105,11 @@ int main() {
 				room = PRODUCE;
 			else if (input == "south")
 				room = 6;
-			else
+			else {
 				cout << "Sorry, thats not an option" << endl;
+				health++;
+			}
 			break;
-
 		case 6:
 			cout << "You are in Aisle 6, You can go north, west, or south" << endl;
 			cin >> input;
@@ -103,8 +129,10 @@ int main() {
 				else if (input == "food court")
 					room = FOOD_COURT;
 
-				else
-					cout << "sorry, thats not an option" << endl;
+				else {
+					cout << "Sorry, thats not an option" << endl;
+					health++;
+				}
 				break;
 			}
 
@@ -113,7 +141,7 @@ int main() {
 			break;
 
 		case ENTRANCE://Entrance
-			cout << "you are walking through the entrance, you can go west, north and east" << endl;
+			cout << "\nyou are walking through the entrance, you can go west, north and east" << endl;
 			cin >> input;
 			if (input == "west")
 				room = CARTS;
@@ -121,8 +149,10 @@ int main() {
 				room = 6;
 			else if (input == "east")
 				room = FOOD_COURT;
-			else
-				cout << "sorry, thats not an option" << endl;
+			else {
+				cout << "Sorry, thats not an option" << endl;
+				health++;
+			}
 			break;
 
 		case FOOD_COURT://Food court
@@ -132,8 +162,10 @@ int main() {
 				room = 6;
 			else if (input == "west")
 				room = ENTRANCE;
-			else
-				cout << "sorry, thats not an option" << endl;
+			else {
+				cout << "Sorry, thats not an option" << endl;
+				health++;
+			}
 			break;
 
 		case CARTS: //Carts
@@ -145,8 +177,10 @@ int main() {
 				room = PRODUCE;
 			else if (input == "east")
 				room = ENTRANCE;
-			else
-				cout << "sorry, thats not an option" << endl;
+			else {
+				cout << "Sorry, thats not an option" << endl;
+				health++;
+			}
 			break;
 
 		case CHECKOUT://Checkout
@@ -156,15 +190,16 @@ int main() {
 				room = 3;
 			else if (input == "east")
 				room = CARTS;
-			else
-				cout << "sorry, thats not an option" << endl;
+			else {
+				cout << "Sorry, thats not an option" << endl;
+				health++;
+			}
 			break;
-
-		case PRODUCE:
 			cout << "You are in the produce, you can go west, east, south or north" << endl;
 			cin >> input;
 			if (input == "west") {
-				cout << "Do you want to go in Aisle 1, 2, or 3?";
+				cout << "Do you want to go in Aisle 1, 2, or 3?" << endl;
+				cin >> input;
 				if (input == "1")
 					room = 1;
 
@@ -174,13 +209,14 @@ int main() {
 				else if (input == "3")
 					room = 3;
 
-				else
-					cout << "sorry, thats not an option" << endl;
+				else {
+					cout << "Sorry, thats not an option" << endl;
+					health++;
+				}
 				break;
 			}
-
 			else if (input == "east") {
-				cout << "Do you want to go in Aisle 4, 5, or 6?";
+				cout << "Do you want to go in Aisle 4, 5, or 6?" << endl;
 				cin >> input;
 
 				if (input == "4")
@@ -199,8 +235,10 @@ int main() {
 			else if (input == "north")
 				room = BACK_ROOMS;
 
-			else
-				cout << "sorry, thats not an option" << endl;
+			else {
+				cout << "Sorry, thats not an option" << endl;
+				health++;
+			}
 			break;
 
 		case PHARMACY:
@@ -212,19 +250,23 @@ int main() {
 			else if (input == "east")
 				room = BACK_ROOMS;
 
-			else
-				cout << "sorry, thats not an option" << endl;
+			else {
+				cout << "Sorry, thats not an option" << endl;
+				health++;
+			}
 			break;
 
 		case BAKERY:
-			cout << "You are in the Backery, you can go south" << endl;
+			cout << "\nYou are in the Backery, you can go south" << endl;
 			cin >> input;
 
 			if (input == "south")
 				room = 4;
 
-			else
-				cout << "sorry, thats not an option" << endl;
+			else {
+				cout << "Sorry, thats not an option" << endl;
+				health++;
+			}
 			break;
 
 		case BACK_ROOMS:
@@ -240,12 +282,31 @@ int main() {
 			else if (input == "east")
 				room = BACK_ROOMS;
 
-			else
+			else {
 				cout << "Sorry, thats not an option" << endl;
+				health++;
+			}
 			break;
 
-		case ENTRANCE:
-			cout << "Welcome little cuh's yo.  "
+		case START:
+			cout << "Welcome to Cart Craze!" << endl;
+			//Ascii Art ------------------------------------------------
+			cout << "    ______ " << endl;
+			cout << "   /|_||_\\`.__ " << endl;
+			cout << "  (.  _    _ _\\ " << endl;
+			cout << "===\`-(_)--(_)- \'===\n\n\n\n";
+			
+			//Decribes rules
+			cout << "Move around the store using the comands to collect items off your list" << endl;
+			cout << "you have enough health for 10 moves so make sure you go to the food court to keep your heath up. \n\n" << endl;
+
+			//Shopping list
+			system("pause");
+			cout << "Heres your shopping list" << endl;
+			ListGen();
+			room = ENTRANCE;
+			//Time Limit
+			timerRunning = true;
 
 		}
 	}
@@ -253,13 +314,12 @@ int main() {
 }
 
 void ListGen() {
-
 	srand(time(NULL));
 
 	string list[5];
 	string foods[30] = {"Strawberries", "Carrots", "Bananas", "Tomatoes", "Broccoli",
 						"Milk", "Cheddar Cheese", "Yogurt", "Butter", "Eggs",
-						"Carrots", "Shrimp", "Chicken Breasts", "Croissants", "Bagels"
+						"Carrots", "Shrimp", "Chicken Breasts", "Croissants", "Bagels",
 						"Frozen Pizza", "Ice Cream", "Canned Corn", "Wheat Bread","Salt", 
 						"Chips", "Chocolate Bars", "Coffee Beans", "Sparkling Water","Olive Oil", 
 						"Cough Medicine", "Tylenol", "DayQuil", "NyQuil", "Vitimins"
@@ -271,6 +331,7 @@ void ListGen() {
 		list[i] = foods[num];
 		printf("Item %d: %s\n", i + 1, list[i].c_str());
 	}
+	cout << "\n" << endl;
 
 } //end of List generator-----------------------------------------------
 
